8e89fe4b95c3fb153105ea17ba40affa1af0c997
VijayaRaghavanM <mvijayaraghavan29@gmail.com>
2020-09-11 16:26:32 +0530

    Fix bugs in code

--- scraper.py
+++ scraper.py
@@ -1,54 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import re
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
 
-data = {"name":[], "label":[], "price":[]}
-term = input("->")
+matplotlib.use('TkAgg')
 
-amazon_data = BeautifulSoup(requests.get(url='https://www.amazon.in/s', params={'k':term}).text, features="html.parser")
-for i in amazon_data.findAll('div', attrs={'class': 's-include-content-margin'}):
-    item = i.find('span', attrs={'class':'a-text-normal'})
-    price = i.find('span', attrs={'class':'a-price-whole'})
+data = []
+search_term = input("Enter product name:")
+
+amazon_data = BeautifulSoup(requests.get(url='https://www.amazon.in/s', params={'k':search_term}).text, features="html.parser")
+items = amazon_data.findAll('div', attrs={'class': 's-include-content-margin'})
+
+for item in items:
+    name = item.find('span', attrs={'class':'a-text-normal'})
+    price = item.find('span', attrs={'class':'a-price-whole'})
     if price is not None:
-        data["label"].append("amazon")
-        data["name"].append(item.get_text())
-        data["price"].append(int(re.sub("\D", "", price.get_text())))
+        temp = {}
+        temp["label"] = "amazon"
+        temp["name"] = name.get_text()
+        temp["price"] = int(re.sub(r"\D", "", price.get_text()))
+        data.append(temp)
         break
     
 
-term = data[0]["name"] if len(data)==1 else term
+search_term = data[0]["name"] if len(data)==1 else search_term
 
-flipkart_data = BeautifulSoup(requests.get(url='https://www.flipkart.com/search', params={'q': term}).text, features="html.parser")
-for a in flipkart_data.findAll('a',href=True, attrs={'class':'_31qSD5'}):
-    name=a.find('div', attrs={'class':'_3wU53n'})
-    price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+flipkart_data = BeautifulSoup(requests.get(url='https://www.flipkart.com/search', params={'q': search_term}).text, features="html.parser")
+items = flipkart_data.findAll('a',href=True, attrs={'class':'_31qSD5'})
+
+for item in items:
+    name=item.find('div', attrs={'class':'_3wU53n'})
+    price=item.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
     if name is not None and price is not None:
-        data["label"].append("flipkart")
-        data["name"].append(item.get_text())
-        data["price"].append(int(re.sub("\D", "", price.get_text())))
+        temp = {}
+        temp["label"] = "flipkart"
+        temp["name"] = name.get_text()
+        temp["price"] = int(re.sub(r"\D", "", price.get_text()))
+        data.append(temp)
         break
 
-paytm_data = BeautifulSoup(requests.get(url='https://paytm.com/shop/search', params={'q':term}).text, features="html.parser")
-for a in paytm_data.findAll('div', attrs={'class':'_2i1r'}):
-    name=a.find('div', attrs={'class':'_2apC'})
-    price=a.find('span', attrs={'class':'_1kMS'})
+paytm_data = BeautifulSoup(requests.get(url='https://paytm.com/shop/search', params={'q':search_term}).text, features="html.parser")
+items = paytm_data.findAll('div', attrs={'class':'_2i1r'})
+
+for item in items:
+    name=item.find('div', attrs={'class':'_2apC'})
+    price=item.find('span', attrs={'class':'_1kMS'})
     if name is not None and price is not None:
-        data["label"].append("paytm")
-        data["name"].append(item.get_text())
-        data["price"].append(int(re.sub("\D", "", price.get_text())))
+        temp = {}
+        temp["label"] = "paytm"
+        temp["name"] = name.get_text()
+        temp["price"] = int(re.sub(r"\D", "", price.get_text()))
+        data.append(temp)
         break
 
-df = pd.DataFrame(data)
-print(df)
-x = df.iloc[:,1], y = df.iloc[:,2]
-x_pos = np.arange(len(x))
-plt.bar(x_pos, y, color='#7ed6df')
+for item in data:
+    print(item["label"], item["name"], item["price"])
+
+x = list(map(lambda item: str(item['label']) + ' - ' + str(item['name']), data))
+y = list(map(lambda item: item['price'], data))
+plt.bar(x,y, bottom=0)
 plt.xlabel("Websites")
 plt.ylabel("Price")
 plt.title("Online Price Comparison")
-plt.xticks(x_pos, x)
 plt.show()
-
